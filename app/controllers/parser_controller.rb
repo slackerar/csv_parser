82ef105039b495a2db9c947176a65113a4d071c7
vladimirchikurov <vladimir.chikurov@s-telematics.ru>
2019-03-15 12:36:33 +0300

    Add controller parser

---
+++ app/controllers/parser_controller.rb
@@ -0,0 +1,4 @@
+class ParserController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'parser/index'
+
+  root 'parser#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
